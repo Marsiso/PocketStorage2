696a15008c273da7142d92ae94fdfa443c817668
Marek Olsak <m_olsak@utb.cz>
2023-03-18 15:14:50 +0100

    Clean architecture project structure missing layers added

---
+++ src/Core/Domain/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Json;
+
+namespace Domain;
+
+public sealed class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public static async Task<WeatherForecast[]?> GetWeatherForecasts(HttpClient Http)
+    {
+        return await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+    }
+}
